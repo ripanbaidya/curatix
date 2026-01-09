b8a51d1e6829a23a79c4ab8c1c4ed70bc2ebc186
Ripan Baidya <ripanbaidya7980@gmail.com>
2026-01-10 01:16:29 +0530

    feat: update main class

--- backend/src/main/java/com/curatix/BackendApplication.java
+++ backend/src/main/java/com/curatix/BackendApplication.java
@@ -2,8 +2,10 @@ package com.curatix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "com.curatix.config.properties")
 public class BackendApplication {
 
     public static void main(String[] args) {
