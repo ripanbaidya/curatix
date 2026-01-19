70acbeb9b6a83ef8f160b1104e82dd82c7d1b2a7
Ripan Baidya <ripanbaidya7980@gmail.com>
2026-01-19 23:18:33 +0530

    feat(security): add policies for password configuration

---
+++ backend/src/main/java/com/curatix/config/security/PasswordConfig.java
@@ -0,0 +1,18 @@
+package com.curatix.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Configuration for password policies
+ */
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+}
