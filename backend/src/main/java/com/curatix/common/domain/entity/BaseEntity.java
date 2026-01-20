4587c398abdc081778de25ada827e3415888e631
Ripan Baidya <ripanbaidya7980@gmail.com>
2026-01-20 17:29:38 +0530

    feat(entity): add BaseEntity

---
+++ backend/src/main/java/com/curatix/common/domain/entity/BaseEntity.java
@@ -0,0 +1,44 @@
+package com.curatix.common.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+import static jakarta.persistence.GenerationType.UUID;
+
+/**
+ * Base entity for all entities
+ */
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = UUID)
+    private String id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", insertable = false)
+    private Instant updatedAt;
+
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by", insertable = false)
+    private String updatedBy;
+}
